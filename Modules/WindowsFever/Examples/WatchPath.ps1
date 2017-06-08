9329617fbcaa751216f663f8d2ebbbe4681ff9be
Claudio Spizzi <claudio.spizzi@arcade.ch>
2017-06-09 01:04:16 +0200

    Fix Write-Host script analyzer issue

--- Modules/WindowsFever/Examples/WatchPath.ps1
+++ Modules/WindowsFever/Examples/WatchPath.ps1
@@ -1,6 +1,6 @@
 
 # Watch the C:\Demo\file.txt for changes and write them to the host.
-Start-WatchPath -Path 'C:\Demo' -Filter 'file.txt' -ChangedAction { Write-Host "File changed: $($Event.SourceArgs[1].FullPath)" }
+Start-WatchPath -Path 'C:\Demo' -Filter 'file.txt' -ChangedAction { $Event.SourceArgs[1].FullPath | Out-File -FilePath 'C:\Log\changed.log' }
 
 # Stop watching on C:\Demo.
 Stop-WatchPath -Path 'C:\Demo'
